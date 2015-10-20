92e27aaf19ba32efa3059792718df2e637354ad8
Jason Parent <ParentJ@advisory.com>
2015-10-20 19:49:15 -0400

    Added media files.

--- bottle_battle/urls.py
+++ bottle_battle/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
     # url(r'^api/v1/meals/', include('meals.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# Serves static files in development environment...
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serves media files in development environment...
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
